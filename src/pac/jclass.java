db7cec5f447dc50bb75f2d502145abd1d3d50132
daniel levy <hackeru_t@outlook.com>
2022-06-22 12:53:21 +0300

    Initial commit

---
+++ src/pac/jclass.java
@@ -0,0 +1,9 @@
+package pac;
+
+public class jclass {
+    public static void main(String[] args) {
+        for (int i = 0; i < 300; i++) {
+            System.out.println(i+2);
+        }
+    }
+}
