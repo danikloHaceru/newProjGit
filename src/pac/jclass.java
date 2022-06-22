7fa43f005e66bb7cc643f968cbb272ec0151a6f9
daniel levy <hackeru_t@outlook.com>
2022-06-22 16:26:48 +0300

    my commit

--- src/pac/jclass.java
+++ src/pac/jclass.java
@@ -3,7 +3,7 @@ package pac;
 public class jclass {
     public static void main(String[] args) {
         for (int i = 0; i < 300; i++) {
-            System.out.println(i+9);
+            System.out.println(i+15);
         }
     }
 }
